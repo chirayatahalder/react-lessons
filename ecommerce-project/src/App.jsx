dc3ce056ebdc3e85f9769b702e5200441649378e
Chirayata Halder <hrijuchira@gmail.com>
2026-05-26 00:15:57 +0530

    lesson 6 checkout page implementation done

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
+import CheckoutPage from "./pages/CheckoutPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/checkout" element={<div>Hello Checkout</div>}/>
+      {/* <Route path="/" element={<HomePage />} /> */}
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
     </Routes>
   );
 }
