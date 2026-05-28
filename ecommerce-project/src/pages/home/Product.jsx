78a24907062c1108208f53a417c7ce27331f739f
Chirayata Halder <hrijuchira@gmail.com>
2026-05-29 02:14:47 +0530

    Lesson 9 pushed with a unsolved bug

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -21,7 +21,7 @@ export default function Product({ product, loadCartItems }) {
   const selectQuantity = (e) => setQuantity(Number(e.target.value));
 
   return (
-    <div className="product-container">
+    <div className="product-container" data-testid="product-container">
       <div className="product-image-container">
         <img className="product-image" data-testid="product-image" src={product.image} />
       </div>
